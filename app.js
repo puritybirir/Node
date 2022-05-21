9a93c89c62311a578aaa3532f51abde342210dd5
puritybirir <puritybirir@gmail.com>
2022-05-21 14:55:41 +0300

    All the node routes

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-const http = require('http');
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/add-product', (req, res, next) => {
-  console.log("In another middleware");
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button><form>')
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-})
-
-app.use('/', (req, res, next) => {
-  console.log("In another middleware");
-  res.send('<h1>Hello from express</h1>')
-});
-
 
 app.listen(3000);
